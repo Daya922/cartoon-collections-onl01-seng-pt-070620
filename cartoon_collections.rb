b8d2b9b03434a1212aa70bd7f71db45bc79be9b3
Dayami Velazquez <dayami.velazquez@gmail.com>
2020-07-19 21:08:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,21 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
-end
+dwarves = ["Doc", "Dopey","Bashful","Grumpy"]
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+call_screams = ["bo", "bam","boom","bazam", "powzer"]
 
-def summon_captain_planet# code an argument here
+def roll_call_dwarves(array)# code an argument here
   # Your code here
+  i= 0 
+  while i < array.length 
+  puts "#{i+1 }. #{array[i]}"
+  i += 1 
+end
+  end
+  
+  roll_call_dwarves(dwarves)
+
+def summon_captain_planet(array) # code an argument here
+  # Your code here
+  
 end
 
 def long_planeteer_calls# code an argument here
