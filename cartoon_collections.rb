95ec0102f12ff0bc786db1625271900dacc28b78
Dayami Velazquez <dayami.velazquez@gmail.com>
2020-07-19 21:34:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -37,8 +37,15 @@ def long_planeteer_calls(array)# code an argument here
 end
 long_planeteer_calls(call_screams)
 
+cheese_types = ["cheddar", "gouda", "camembert"]
+snacks = ["crackers", "gouda", "thyme"]
 
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |type|
+    cheese_types.include?(type)
+  end
 end
+
+find_the_cheese(snacks)
